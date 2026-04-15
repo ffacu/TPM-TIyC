94eb93c32ee8f9fee9595d70fe3c63c6321b52d5
Paez Juan <juancruzpaez030@gmail.com>
2026-04-14 21:38:40 -0300

    hamming function implemented: errors detected were commented out in the code

--- TPM1 Protección de Archivos/pm1/src-tauri/src/file_editor.rs
+++ TPM1 Protección de Archivos/pm1/src-tauri/src/file_editor.rs
@@ -1,43 +1,80 @@
 use std::io::{self, Read, Write};
 
-pub fn hamming(block_size_bits: isize, input: &mut std::fs::File, output: &mut std::fs::File) -> io::Result<()> {
+pub fn hamming(block_size_bits: i32, input: &mut std::fs::File, output: &mut std::fs::File) -> io::Result<()> {
+    
     // Read the entire file into a byte vector
     let mut buffer =  Vec::new();
-    input.read_to_end(&mut buffer)?;
+    let mut codeword = Vec::new();
 
-    let mut encoded_stream = Vec::new();
+    input.read_to_end(&mut buffer)?; //read the file input into the buffer vector (read bytes not bits, miss the conversion)
 
-    match block_size_bits {
-        8 => {
-            // TODO: Implement 8-bit logic
-            // 1. Read 4 bits (half a byte) of data from 'buffer'
-            // 2. Calculate parity bits for positions 1, 2, and 4.
-            // 3. Calculate the overall parity bit for position 0 (Extended Hamming).
-            // 4. Pack the 4 data bits and 4 parity bits into a single u8.
-            // 5. Push to 'encoded_stream'.
-        },
-        1024 => {
-            // TODO: Implement 1024-bit logic
-            // 1. Read 1013 bits of data.
-            // 2. Insert parity bits at positions 1, 2, 4, 8 ... 512.
-            // 3. Pack into 128 bytes.
-        },
-        16384 => {
-            // TODO: Implement 16384-bit logic
-            // 1. Read 16369 bits of data.
-            // 2. Insert parity bits at powers of 2 up to 8192.
-            // 3. Pack into 2048 bytes.
-        },
-        _ => {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "Unsupported block size. Must be 8, 1024, or 16384.",
-            ))
+    let mut control_bits_quantity = 0;  
+    
+    //calculate the required control bits for the block size
+    while 1 << control_bits_quantity <= block_size_bits + control_bits_quantity + 1 {
+        control_bits_quantity = control_bits_quantity + 1;
+    }
+    
+    let info_bits_quantity = (block_size_bits - 1 - control_bits_quantity).try_into().unwrap();
+
+    let mut internal_codeword = vec![0; (block_size_bits - 1).try_into().unwrap()];    
+    
+    //one iteration is for hamminizing each file block (the amount of bits of information is taken), check if this correctly implemented!
+    //the information is taken in bytes, no bits, this implementation is partcially incorrect
+    for i in (0..(buffer.len())).step_by(info_bits_quantity) { 
+
+        let mut parity_bits = 0; 
+        let mut step_buffer = i;    
+
+        //initialize the control bits and add the info bits
+        for j in 1..(info_bits_quantity - 1) as usize {
+
+            
+            if j == 1 << parity_bits{
+                
+                internal_codeword[j] = 0; //parity placeholder
+                parity_bits += 1;
+
+            }
+            else{
+
+                internal_codeword[j] = buffer[step_buffer]; //information bit placeholder
+                step_buffer += 1;
+            
+            }
         }
+
+        //calculate control bits
+        for j in 0..(control_bits_quantity-1){
+            let parity_position: usize = 1 << j;
+            let mut parity_value = 0;
+
+            for bit_position in 1..(block_size_bits as usize - 1){
+                
+                if (bit_position & parity_position) != 0 {
+    
+                    parity_value = parity_value ^ internal_codeword[bit_position];
+    
+                }
+        
+            }
+
+            internal_codeword[parity_position] = parity_value;
+        }
+
+        //The last parity bit check has not been calculated
+
+        // append the currently block to the output file
+        codeword.append(&mut internal_codeword);
+
+        // The vector needs to be reinitialized.
+
     }
 
+    // the information needs to be packaged into byte vector
+    
     // Write the fully processed binary stream to the output file
-    output.write_all(&encoded_stream)?;
+    output.write_all(&internal_codeword)?;
     
     Ok(())
 }
